52bc5c16a41831d9bf53f78846e33b2289c4b11c
Ivan De Martino <ivan.martino@outlook.com>
2022-07-23 08:26:33 -0300

    add base project

---
+++ FlashPopsGame/FlashPopsGame/FlashPopsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashPopsGameApp.swift
+//  FlashPopsGame
+//
+//  Created by Ivan De Martino on 7/23/22.
+//
+
+import SwiftUI
+
+@main
+struct FlashPopsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
